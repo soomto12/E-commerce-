f3ec0211c221f7042a5842af8f870ec2c8e4c74a
unknown <edokasomtochukwu@gmail.com>
2025-06-23 20:16:50 +0100

    store my cart in a fire base backend

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 function App() {
  const showCart = useSelector(state=>state.ui.cartIsVisible)
+
+ const cart = useSelector(state=>state.cart);
+
+useEffect(()=>{
+  fetch("https://cart-48f1d-default-rtdb.firebaseio.com/Cart.json"
+,{
+   method:"PUT",
+   body:JSON.stringify(cart),
+ }
+   
+)
+},[cart]);
+
   return (
     <Layout>
-
      {showCart && <Cart /> }
       <Products />
     </Layout>
